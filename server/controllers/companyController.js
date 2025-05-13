e5f2e345ca41bbfedd069764f4b322bea7b8349e
Keerthan A <keerthann004@gmail.com>
2025-05-13 20:00:26 +0530

    Second Commit

---
+++ server/controllers/companyController.js
@@ -0,0 +1,41 @@
+
+// Register a new Company
+export const registerCompany = async (req,res) => {
+    
+
+}
+
+// Company login
+export const loginCompany = async (req,res) => {
+
+}
+
+// Get company data
+export const getCompanyData = async (req,res) => {
+
+}
+
+// Post a new job
+export const postJob = async (req,res) => {
+
+}
+
+// Get Company Job Applications
+export const getCompanyJobApplicants = async (req,res) => {
+
+}
+
+// Get Company Posted Jobs
+export const getCompanyPostedJobs = async (req,res) => {
+
+}
+
+// Change Job Application Status
+export const ChangeJobApplicationsStatus = async (req,res) => {
+
+}
+
+// Change job visiblity
+export const changeVisiblity = async (req,res) => {
+
+}
