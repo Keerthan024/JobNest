ce92b8ef8bd1e0705796d07692e2cde421085887
Keerthan A <keerthann004@gmail.com>
2025-05-18 10:57:46 +0530

    Updated

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -1,5 +1,7 @@
 import Company from "../models/Company.js";
 import bcrypt from 'bcrypt';
+import { v2 as cloudinary } from 'cloudinary'
+import generateToken from "../utils/generateToken.js";
 
 // Register a new Company
 export const registerCompany = async (req,res) => {
@@ -23,10 +25,29 @@ export const registerCompany = async (req,res) => {
         const salt = await bcrypt.genSalt(10)
         const hashPassword = await bcrypt.hash(password, salt)
 
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path)
+
+        const company = await Company.create({
+            name,
+            email,
+            password: hashPassword,
+            image: imageUpload.secure_url
+        })
+
+        res.json({
+            success: true,
+            company: {
+                _id: company._id,
+                name: company.name,
+                email: company.email,
+                image: company.image
+            },
+            token: generateToken(company._id)
+        })
         
 
     } catch (error) {
-        
+        res.json({success:false, message: error.message})
     }
 
 }
